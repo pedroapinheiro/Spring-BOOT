9eb19d91cb443affb4e5de9dc757d4f6339bfd44
unknown <pedro.azevedop18@hotmail.com>
2023-03-30 14:04:57 -0300

    Deploy do Projeto Blog Pessoal

--- blogpessoal1/src/main/java/com/generation/blogpessoal1/model/Usuario.java
+++ blogpessoal1/src/main/java/com/generation/blogpessoal1/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -27,6 +29,7 @@ public class Usuario {
 	@NotNull(message = "O atributo Nome é Obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
@@ -42,6 +45,8 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	/* Métodos Construtores  */
+
 	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
 		this.id = id;
 		this.nome = nome;
@@ -52,6 +57,8 @@ public class Usuario {
 	
 	public Usuario() { }
 	
+	/* Insira os Getters and Setters */
+
 
 	public Long getId() {
 		return this.id;
